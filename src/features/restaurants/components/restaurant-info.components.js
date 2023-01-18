1a12ba346d80dbf55b6d32db86fd1751197a83ae
hahuyhoang <huyhoang24062000@gmail.com>
2023-01-18 23:41:40 +0700

    ad our theme

--- src/features/restaurants/components/restaurant-info.components.js
+++ src/features/restaurants/components/restaurant-info.components.js
@@ -2,23 +2,28 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import styled from "styled-components";
 import { Button, Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
+import { Image } from "react-native";
+import { Spacer } from "../../../components/spacer/spacer.components";
 
 const RestaurantCart = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 const RestaurantCartCover = styled(Card.Cover)`
-  padding: ${(props) => props.theme.space[3]};
+  padding: ${(props) => props.theme.space[2]};
   background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const Title = styled(Text)`
   color: ${(props) => props.theme.colors.ui.primary};
-  font-family: ${(props) => props.theme.fonts.heading};
+  font-family: ${(props) => props.theme.fonts.body};
   font-size: ${(props) => props.theme.fontSizes.body};
 `;
 
 const Info = styled(View)`
-  padding: ${(props) => props.theme.space[2]};
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 const Address = styled(Text)`
@@ -26,23 +31,65 @@ const Address = styled(Text)`
   font-size: ${(props) => props.theme.fontSizes.caption};
 `;
 
+const Ratting = styled(View)`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+const Section = styled(View)`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SectionEnd = styled(View)`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+const Open = styled(SvgXml)``;
+
 const RestaurantInfo = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
-    icon,
+    icon = [
+      "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+    ],
     photos = [
       "https://images.pexels.com/photos/291528/pexels-photo-291528.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     ],
-    address = " 100 some random street",
+    address = "100 some random street",
     isOpenNow = true,
     rating = 4,
-    isCloseTemporarily,
+    isCloseTemporarily = true,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
   return (
     <RestaurantCart elevation={5}>
       <RestaurantCartCover source={{ uri: photos[0] }} />
       <Info>
         <Title>{name}</Title>
+        <Section>
+          <Ratting>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Ratting>
+          <SectionEnd>
+            {isCloseTemporarily && (
+              <Text variant="label" style={{ color: "red" }}>
+                CLOSE TEMPORARILY
+              </Text>
+            )}
+            <Spacer variant={'left.large'} />
+            {isOpenNow && <Open xml={open} width={20} height={20} />}
+            <Spacer variant={'left.large'} />
+            <Image style={{ width: 15, height: 15 }} source={icon} />
+          </SectionEnd>
+        </Section>
         <Address>{address}</Address>
       </Info>
     </RestaurantCart>
